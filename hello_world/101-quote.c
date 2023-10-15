83099dfadd75f2a5b4ec1a53751b43c63f304d0e
Chaima Ben Slima <chaimabslima@outlook.fr>
2023-10-15 12:33:51 -0700

    a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,6 +6,10 @@
  *
  * Return: Always 0.
  */
+void putchar(char c) {
+  extern long write(int, const char *, unsigned long);
+  (void) write(1, &c, 1);
+}
 int main(void)
 {
 	const char msg[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
