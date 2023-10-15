7ae361d900985226b03e3fbe6a9847b34736ca1c
Chaima Ben Slima <chaimabslima@outlook.fr>
2023-10-15 12:44:48 -0700

    a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,16 +1,10 @@
 #include <unistd.h>
-#include <sys/syscall.h>
 /**
- * main - Entry point
- *                
- *
- * Return: Always 0.
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-	const char msg[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	/*const char msg1[]="\n";*/
-        syscall(SYS_write,1,msg,sizeof(msg));
-	/*syscall(SYS_write,1,msg1,sizeof(msg1));*/
-        return (0);	
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
