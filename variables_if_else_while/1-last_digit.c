07b9438a2648d7b5009cade5f41ee990a99f3ac2
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-16 12:26:28 +0100

    print the last digit of the number stored in the variable n.

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,41 +2,44 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: 0
-*/
-int lastdight(int n){
-    while (n>10){
-        n=n%10;
-    }
-    return (n);
+ * main - Entry point
+ * lastdight -Give the last dight of a number
+ * Return: 0
+ */
+int lastdight(int n)
+{
+	while (n > 10)
+	{
+		n = n % 10;
+	}
+	return (n);
 }
 
 int main(void)
 {
 	int n;
-    int a;
-    /*
-	* A random value stored on the variable n
-	*/
+	int a;
+	/*
+	 * A random value stored on the variable n
+	 */
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    a= lastdight(abs(n));
-    if ((a>5)&& (n>0))
-    {
-        printf("Last digit of %d is %d and is greater than 5\n",n,a);
-    }
-    else if((a<6) && (n>0) && (a!=0))
-    {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n",n,a);
-    }
-    else if (n<0)
-    {
-        printf("Last digit of %d is -%d and is less than 6 and not 0\n",n,a);
-    }
-    else{
-        printf("Last digit of %d is 0 and is 0\n",n);
-    }
-    return(0);
+	a = lastdight(abs(n));
+	if ((a > 5) && (n > 0))
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+	else if ((a < 6) && (n > 0) && (a != 0))
+	{
+		printf("beyLast digit of %d is %d and is less than 6 and not 0\n", n, a);
+	}
+	else if (n < 0)
+	{
+		printf("Last digit of %d is -%d and is less than 6 and not 0\n", n, a);
+	}
+	else
+	{
+		printf("Last digit of %d is 0 and is 0\n", n);
+	}
+	return (0);
 }
