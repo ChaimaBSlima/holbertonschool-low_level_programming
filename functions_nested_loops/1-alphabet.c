2ebc0fcaaa0a4165ef44298fa7607e5dabc96ef2
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-17 09:51:16 +0100

    a function that prints the alphabet, in lowercase, followed by a new line.

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,6 +5,16 @@
  *
  * Return: Always 0.
  */
+void print_alphabet(void){
+    int i;
+	char j;
+
+	for (i = 97; i < 123; i++)
+	{
+		j = i;
+		_putchar(j);
+	}
+}
 int main(void)
 {
     print_alphabet();
