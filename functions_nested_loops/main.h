23392126502b09c4f5fe587e81ddef36073d7639
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-17 09:48:06 +0100

    main file

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,15 @@
-#include <stdio.h>
-void chaima(char text[]){
-    printf(text[])
+#include <unistd.h>
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+void print_alphabet(void){
+    int i;
+	char j;
+
+	for (i = 97; i < 123; i++)
+	{
+		j = i;
+		_putchar(j);
+	}
 }
