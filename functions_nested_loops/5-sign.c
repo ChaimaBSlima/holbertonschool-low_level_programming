bc4022f6eecb6b041e21381b8fe9f4ce52412735
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-17 11:57:31 +0100

    a function that prints the sign of a number.

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isalpha - prints the sign of a number.
+ * Description:Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+ * @n:The code ASCII of the charcter
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	if (n>0)
+	{
+        _putchar("+");
+        return (1);
+	}
+	else if (n<0)
+	{
+        _putchar("-");
+		return (-1);
+        
+	}else{
+        _putchar("0");
+		return (0);
+    }
+}
