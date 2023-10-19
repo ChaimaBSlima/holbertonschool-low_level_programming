7aeb24b54b9f0dc3411e7b2a96087b5245f72b81
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-19 09:44:17 +0100

    Do not print 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,16 @@
  */
 void print_most_numbers(void)
 {
-    int i;
-    char j;
+	int i;
+	char j;
 
-    for (i = 48; i < 58; i++)
-    {
-        if ((i != 50) && (i != 52))
-        {
-            j = i;
-            putchar(j);
-        }
-    }
-    putchar('\n');
+	for (i = 48; i < 58; i++)
+	{
+		if ((i != 50) && (i != 52))
+		{
+			j = i;
+			putchar(j);
+		}
+	}
+	putchar('\n');
 }
