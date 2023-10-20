983bc887e2659f117b8d59f0562629b036e12f70
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-20 09:15:58 +0100

    a program that prints all possible different combinations of two digits.*

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int a;
+
+	for (i = 0; i < 8; i++)
+	{
+
+		for (j = i + 1; j < 9; j++)
+		{
+			for (a = j + 1; a < 10; a++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(a + '0');
+				if (i != 7 || j != 8 || a != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
