310b236898dbf342cec01ed585e6368ab0862aba
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-21 16:36:41 +0100

    a program that finds and prints the sum of the even-valued terms, followed by a new line.

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long i;
+	unsigned long j;
+	unsigned long s;
+
+	i = 1;
+	j = 2;
+	s = 0;
+	printf("%lu, ", i);
+	printf("%lu, ", j);
+	while (s < 4000000)
+	{
+		s = i + j;
+		if (s % 2 == 0)
+		{
+			if (k != 50)
+			{
+				printf("%lu, ", s);
+			}
+			else
+			{
+				printf("%lu", s);
+			}
+		}
+		i = j;
+		j = s;
+	}
+	printf("\n");
+	return (0);
+}
