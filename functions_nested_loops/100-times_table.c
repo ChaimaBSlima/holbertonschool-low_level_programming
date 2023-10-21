8713b3f0d08e90d5d8c1ec439f3d0908ce709601
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-21 15:35:22 +0100

    a function that prints the n times table, starting with 0.

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -10,46 +10,48 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
-
-	for (i = 0; i < n + 1; i++)
+	if (n < 16 && n > -1)
 	{
-		for (j = 0; j < n + 1; j++)
+		for (i = 0; i < n + 1; i++)
 		{
-			if (i * (j + 1) < 10)
+			for (j = 0; j < n + 1; j++)
 			{
-				if (j != n)
+				if (i * (j + 1) < 10)
 				{
-					printf("%d,   ", i * j);
+					if (j != n)
+					{
+						printf("%d,   ", i * j);
+					}
+					else
+					{
+						printf("%d", i * j);
+					}
 				}
-				else
+				else if (i * (j + 1) < 100 && i * (j + 1) > 9)
 				{
-					printf("%d", i * j);
-				}
-			}
-			else if (i * (j + 1) < 100 && i * (j + 1) > 9)
-			{
 
-				if (j != n)
-				{
-					printf("%d,  ", i * j);
+					if (j != n)
+					{
+						printf("%d,  ", i * j);
+					}
+					else
+					{
+						printf("%d", i * j);
+					}
 				}
 				else
 				{
-					printf("%d", i * j);
+					if (j != n)
+					{
+						printf("%d, ", i * j);
+					}
+					else
+					{
+						printf("%d", i * j);
+					}
 				}
 			}
-			else
-			{
-				if (j != n)
-				{
-					printf("%d, ", i * j);
-				}
-				else
-				{
-					printf("%d", i * j);
-				}
-			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 }
