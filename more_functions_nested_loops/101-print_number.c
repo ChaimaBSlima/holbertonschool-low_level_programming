32af343c1eba75a31ccb976ef4a2d654420f3dec
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-22 12:39:10 +0100

    a function that prints an integer.

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,59 @@
 #include "main.h"
 /**
- * print_number - a function that prints an integer.
+ * print_number -  a function that prints an integer.
  * @n: The integer
  * Return: void
  */
 
+int reverse(int n)
+{
+	int rev = 0;
+	int test = 1;
+	int i = 1;
+	while (test)
+	{
+		if (n / (i * 10) > 0)
+		{
+			i *= 10;
+			printf("%d\n", i);
+		}
+		else
+		{
+			test = 0;
+		}
+	}
+	while (n > 0)
+	{
+		rev = rev + ((n % 10) * i);
+		n = n / 10;
+		i++;
+	}
+	return rev;
+}
 void print_number(int n)
 {
-	if (n < 0)
+	if (n > 0)
+	{
+		n = reverse(n);
+		while (n > 0)
+		{
+			_putchar(n % 10 + '0');
+			n = n / 10;
+		}
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
+		n = reverse(n);
+		while (n > 0)
+		{
+			_putchar(n % 10 + '0');
+			n = n / 10;
+		}
 	}
-	if ((n / 10) > 0)
+	else
 	{
-		print_number(n / 10);
+		_putchar('0');
 	}
-
-	_putchar((n % 10) + '0');
 }
