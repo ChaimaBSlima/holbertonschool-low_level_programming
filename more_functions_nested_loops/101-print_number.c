902a30ccfd1b5df535fa94c67b72e5530565be64
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-22 11:29:27 +0100

    a function that prints an integer.

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -4,10 +4,23 @@
  * @n: The integer
  * Return: void
  */
+int reverse(int n)
+{
+	int rev = 0;
+	int i = 0;
+	while (n > 0)
+	{
+		rev = ((n % 10) * (10 * *i));
+		n = n / 10;
+		i++;
+	}
+	return rev;
+}
 void print_number(int n)
 {
 	if (n > 0)
 	{
+		n = reverse(n);
 		while (n > 0)
 		{
 			_putchar(n % 10 + '0');
@@ -18,14 +31,15 @@ void print_number(int n)
 	{
 		_putchar('-');
 		n = n * -1;
+		n = reverse(n);
 		while (n > 0)
 		{
 			_putchar(n % 10 + '0');
 			n = n / 10;
 		}
-	} else
+	}
+	else
 	{
 		_putchar('0');
 	}
-	
 }
