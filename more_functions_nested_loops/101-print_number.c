54b6ea89e7ab2a9519c34290b63af068130ae7d2
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-22 11:16:26 +0100

    a function that prints an integer.

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * print_square - function that prints a square,
- * followed by a new line.
- * @n: The number of #
+ * print_number -  a function that prints an integer.
+ * @n: The integer
  * Return: void
  */
 void print_number(int n)
 { 
     while (n > 0)
     {
-        _putchar( n % 10 + "0");
+        _putchar( n % 10 + '0');
         n = n / 10;
     }
 }
