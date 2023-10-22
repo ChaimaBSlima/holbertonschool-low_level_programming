b007e840aae4ea9b52dcbf209a130c42b9d8c8d9
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-22 11:18:03 +0100

    a function that prints an integer.

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -5,10 +5,13 @@
  * Return: void
  */
 void print_number(int n)
-{ 
-    while (n > 0)
-    {
-        _putchar( n % 10 + '0');
-        n = n / 10;
-    }
+{
+	if (n >= 0)
+	{
+		while (n > 0)
+		{
+			_putchar(n % 10 + '0');
+			n = n / 10;
+		}
+	}
 }
