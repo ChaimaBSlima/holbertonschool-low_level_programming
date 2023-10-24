e7bc7dca14f7066b9f210384329c6a9fb4bf151a
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-24 19:48:55 +0100

    a function that convert a string to an integer.

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,31 +9,23 @@
  */
 int _atoi(char *s)
 {
-	int a;
+	/*int a;
 	int i;
-	int sum;
+	char ch[] = "";
 
 	i = 0;
 	a = 1;
-	sum = 0;
 	while (*(s + i) != '\0')
 	{
+		if (s[i] != '+' && s[i] != '-')
+		{
+			strcat(ch,s[i]);
+		}
 		if (s[i] == '-')
 		{
 			a = a * -1;
 		}
-		else if (*(s+i) >= '0' && *(s+i) <= '9')
-			break;
 		i++;
-	}
-	while (*s >= '0' && *s <= '9')
-	{
-		sum *= 10;
-		sum += *s - '0';
-		s++;
-	}
-	if (a == -1)
-		return (-sum);
-	else
-		return (sum);
+	}*/
+	return (atoi(ch) * a);
 }
