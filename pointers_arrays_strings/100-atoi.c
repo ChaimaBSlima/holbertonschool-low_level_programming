25096be7f2500ad04f2ef1e0af3e7258a81a64ce
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-24 20:24:59 +0100

    a function that convert a string to an integer.

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * _atoi - convert a string representation of an integer to an integer
- *
- * @s: string possibly containing an integer
- *
- * Return: first integer in string
+ * removeChar -a function that removes a character
+ * from a string
+ * @str: the string
+ * @c: the character
+ * Return: void
+ */
+void removeChar(char *str, char c)
+{
+	int i, j;
+	int len = strlen(str);
+
+	for (i = j = 0; i < len; i++)
+	{
+		if (str[i] != c)
+		{
+			str[j++] = str[i];
+		}
+	}
+	str[j] = '\0';
+}
+/**
+ * _atoi -a function that convert a string to an integer.
+ * @s: the string
+ * Return: int value
  */
 int _atoi(char *s)
 {
-	int signs = 1;
-	unsigned int sum = 0;
-	int final = 0;
+	int a;
+	int i;
 
-	for (; *s != '\0'; s++)
+	i = 0;
+	a = 1;
+	while (*(s + i) != '\0')
 	{
-		if (*s == '-')
-			signs *= -1;
-		else if (*s >= '0' && *s <= '9')
-			break;
+		if (s[i] == '-')
+		{
+			a = a * -1;
+		}
+		i++;
 	}
-	while (*s >= '0' && *s <= '9')
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-		sum *= 10;
-		sum += *s - '0';
-		s++;
+		if (s[i] == '-' || s[i] == '+')
+		{
+			removeChar(s, s[i]);
+		}
+		i++;
 	}
-
-	if (signs == -1)
-		final = -sum;
-	else
-		final = sum;
-
-	return (final);
+	return (atoi(s) * a);
 }
