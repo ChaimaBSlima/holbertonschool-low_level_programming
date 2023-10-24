e852a69cdd3321298ce125bde77ce0ef10349cf2
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-24 13:39:37 +0100

    a function that convert a string to an integer.

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strcpy - a function that convert a string to an integer.
+ * @s: the string
+ * Return: int value 
+ */
+int _atoi(char *s)
+{
+	return(atoi(s));
+}
