048271a97d10d3afdec09ae65ef61c7d4752bc1d
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-25 19:09:53 +0100

    a function that capitalizes all words of a string.

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper -a function that changes all lowercase
+ * letters of a string to uppercase.
+ * @s: the string of address s
+ * Return: string value
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
