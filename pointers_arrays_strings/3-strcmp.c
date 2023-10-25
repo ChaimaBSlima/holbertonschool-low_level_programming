b2753eef1541d171a3ce73adacff40b75096c150
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-25 11:31:46 +0100

    a function that compares two strings.

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,24 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * *_strncpy -a function that copies a string.
- * @dest: the string
- * @src: the string
- * @n: int byte
- * Return: string value
+ * *_strcmp -a function that compares two strings.
+ * @s1: the string
+ * @s2: the string
+ * Return: int value
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int a;
 
-	i=0;
+	i = 0;
 	while (s1[i] == s2[i] && s1[i] != '\0')
 	{
 		i++;
 	}
-
 	a = s1[i] - s2[i];
 	return (a);
 }
-
