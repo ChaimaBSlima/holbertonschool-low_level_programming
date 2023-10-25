b9b1686273402ef572aebc197837ffb793cbf8e4
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-25 12:01:32 +0100

    main.h

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  print_rev - prints a string,
+ *  in reverse, followed by a new line.
+ * @s: the string of address s
+ * Return: void
+ */
+char *string_toupper(char *s)
+{
+	char Upp[25] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" char Low[25] = "abcdefghijklmnopqrstuvwxyz" int i;
+	int j;
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (Low[j] != '\0' || Low(j) != s[i])
+		{
+			j++;
+		}
+		s[i] = Upp[j];
+	}
+	return (s);
+}
