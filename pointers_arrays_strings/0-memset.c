aa4c47b7f2fd612bfb66606064a0f7c1a1859d47
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-26 09:57:59 +0100

    a function that fills memory with a constant byte.

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * *_strcat - a function that concatenates two strings.
- * @dest: the string
- * @src: the string
+ * _memset - a function that fills
+ * memory with a constant byte.
+ * @s: the string
+ * @b : the string to add
+ * @n: the size of matrix
  * Return: string value
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
