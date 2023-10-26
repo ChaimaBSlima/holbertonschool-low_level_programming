8979b279ef22cbb3e4e50906538ed95312c7425a
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-26 11:26:58 +0100

    a function that fills memory with a constant byte.

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _memset - a function that fills
+ * memory with a constant byte.
+ * @s: the string
+ * @b : the string to add
+ * @n: the size of matrix
+ * Return: string value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
