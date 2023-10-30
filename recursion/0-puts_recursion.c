8c4ccaa965fb4e8c120feb28db5fc909c7178679
ChaimaBSlima <chaimabslima@outlook.fr>
2023-10-30 09:52:52 +0100

    a function that prints a string, followed by a new line.

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,12 +7,10 @@
  */
 void _puts_recursion(char *s)
 {
-    int i = 0;
-    if (s[i] == '\0')
+    if (*s == '\0')
     {
         return;
     }
-    _putchar(s[i]);
-	 i++;
-    _puts_recursion(s);
+    printf("%c",*s);
+    _puts_recursion(s + 1);
 }
