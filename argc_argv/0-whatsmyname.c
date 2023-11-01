030508464c5b0fa4caefa36ef73be78326a58826
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-01 20:18:01 +0100

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * @argc : size of the array
+ * @argv : elements of the array
  * Return: 0
  */
-int main(int argc, char **argv )
+int main(int argc, char **argv)
 {
-   printf("%s\n",argv[0]);
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
