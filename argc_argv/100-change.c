895bc2c4d5bb6a04930a833aa688dd90a36a44a4
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-03 20:04:24 +0100

    100-change.c

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point 
+ * calcul - find the number of cents
+ * @sum : the number of cents
+ * @cent : the money
+ * Return: int value
+ */
+int calcul(int sum, int cent)
+{
+	while (cent != 0)
+	{
+		if (cent >= 25)
+		{
+			sum += 1;
+			cent = cent - 25;
+		}
+		else if (cent >= 10)
+		{
+			sum += 1;
+			cent = cent - 10;
+		}
+		else if (cent >= 5)
+		{
+			sum += 1;
+			cent = cent - 5;
+		}
+		else if (cent >= 2)
+		{
+			sum += 1;
+			cent = cent - 2;
+		}
+		else if (cent >= 1)
+		{
+			sum += 1;
+			cent = cent - 1;
+		}
+	}
+	return (sum);
+}
+/**
+ * main - Entry point
  * @argc : size of the array
  * @argv : elements of the array
  * Return: 0 or 1
@@ -23,34 +61,7 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		while (cent != 0)
-		{
-			if (cent >= 25)
-			{
-				sum += 1;
-				cent = cent - 25;
-			}
-			else if (cent >= 10)
-			{
-				sum += 1;
-				cent = cent - 10;
-			}
-			else if (cent >= 5)
-			{
-				sum += 1;
-				cent = cent - 5;
-			}
-			else if (cent >= 2)
-			{
-				sum += 1;
-				cent = cent - 2;
-			}
-			else if (cent >= 1)
-			{
-				sum += 1;
-				cent = cent - 1;
-			}
-		}
+		sum = calcul(sum, cent);
 	}
 	printf("%d\n", sum);
 	return (0);
