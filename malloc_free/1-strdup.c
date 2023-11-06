2a2139d556154435751236f5548fc3851b1329ee
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-06 09:23:33 +0100

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_array - a function that creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: The size of the array
+ * @c: the element of the array
+ * Return: the array.
+ */
+char *_strdup(char *str)
+{
+	char *ch;
+	unsigned int i;
+	int n;
+
+	n = strlen(str);
+	ch = (char *)malloc(n + 1 * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (ch != NULL)
+		{
+			for (i = 0; str[i] != '\0'; i++)
+				ch[i] = str[i];
+		}
+	}
+	ch[i] = '\0';
+	return (ch);
+}
