9b4bee314e4813e535f8bc3a208b40a06c2f675b
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-07 09:38:48 +0100

    a function that concatenates two strings.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ch;
 	unsigned int i, j;
 	unsigned int n1 = 0;
-	//unsigned int n2 = 0;
+	/*unsigned int n2 = 0;*/
 
 	if (s1 != NULL)
 	{
