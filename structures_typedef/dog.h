53270c92c6e684c2acacf28f6d9b3609ba9a6315
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-13 09:41:10 +0100

    Define a new type struct dog with the following elements:

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef OBJECT_LIKE_MARCO
 #define OBJECT_LIKE_MARCO
 
+/**
+ * struct dog - frist struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
+
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /*OBJECT_LIKE_MARCO*/
