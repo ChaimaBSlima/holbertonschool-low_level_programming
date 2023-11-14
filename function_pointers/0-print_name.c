c528b5fdcaf7e0c4637a333993d456ac34d02320
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-14 10:01:52 +0100

    a function that prints a name.

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,16 @@
 #include "function_pointers.h"
 
 /**
- * init_dog - a function that initialize a variable of type struct dog
- * @d: the struct
- * @name: the dog name
- * @age: the dog age
- * @owner: the owner name
+ * print_name -  a function that prints a name.
+ * @name: the person name
+ * @f: the function
  *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-  f(name);
+    if (f)
+    {
+        f(name);
+    }
 }
