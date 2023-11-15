7884305d7b48d780c37c4de79826b6123970969b
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-15 09:32:34 +0100

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
