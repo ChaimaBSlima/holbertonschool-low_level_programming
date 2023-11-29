34d586b4d3222f7fcf610316e6b1a0b096deed4e
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-29 10:48:49 +0100

    a function that returns the sum of all the data (n) of a dlistint_t linked list.

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint -a function that returns the sum of all
+ *  the data (n) of a dlistint_t linked list.
+ * @head: pointer
+ *
+ * Return: sum value
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *chaima = head;
+
+	while (cursor)
+	{
+		sum += chaima->n;
+		chaima = chaima->next;
+	}
+	return (sum);
+}
