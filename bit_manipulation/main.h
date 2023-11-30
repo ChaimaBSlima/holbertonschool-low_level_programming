e0cb5a3b3095a017724240d4e7306ad3ef35be5e
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-30 09:23:46 +0100

    bit manipulation

---
+++ bit_manipulation/main.h
@@ -0,0 +1,16 @@
+#ifndef __BIT_MANIPULATION__
+#define __BIT_MANIPULATION__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif /*__BIT_MANIPULATION__*/
