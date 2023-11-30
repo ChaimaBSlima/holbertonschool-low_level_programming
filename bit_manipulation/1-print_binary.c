2fa5fe1295e87b7751d1c8804526cd39ad94d7a5
ChaimaBSlima <chaimabslima@outlook.fr>
2023-11-30 09:48:39 +0100

    1-print_binary.c

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_binary - a function that prints
+ *  the binary representation of a number.
+ * @n: the number
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int flag = 0;
+	int len = 0;
+	int i, a = 1;
+	int b;
+	unsigned int p;
+
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (31 - i)) & n);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (31 - i);
+			putchar(b + 48);
+			len++;
+		}
+	}
+	if (len == 0)
+	{
+		putchar('0');
+	}
+}
