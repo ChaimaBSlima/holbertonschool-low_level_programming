d7e9d0bd7f90fc591745c16cda2b0a77b0a8beba
ChaimaBSlima <chaimabslima@outlook.fr>
2023-12-01 09:50:05 +0100

    main.h

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * get_bit -a function that returns
- * the value of a bit at a given index.
+ * set_bit -  function that sets the
+ * value of a bit to 1 at a given index.
  * @n: the number
  * @index: the given index
  *
