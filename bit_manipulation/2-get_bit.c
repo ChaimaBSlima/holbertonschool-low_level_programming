c7297d8d7e08b7d97829875296dfa0641be57c81
ChaimaBSlima <chaimabslima@outlook.fr>
2023-12-01 09:00:43 +0100

    a function that returns the value of a bit at a given index.

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int i;
 	int *array;
-	int bytes = 0;
+	unsigned int bytes = 0;
 	int the_number;
 	unsigned int el3adad = n;
 	unsigned int num = el3adad;
