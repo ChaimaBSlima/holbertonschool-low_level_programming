23559fb66db289972474812740c41f999986ce9b
ChaimaBSlima <chaimabslima@outlook.fr>
2023-12-04 09:47:39 +0100

    a function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,13 +10,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int length = 0, file;
 
-	length = strlen(text_content);
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "\0";
-
+	length = strlen(text_content);
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
