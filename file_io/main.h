2ec1a82441139367b83984555e638cd9256500b5
ChaimaBSlima <chaimabslima@outlook.fr>
2023-12-04 09:30:00 +0100

    the actual number of letters it could read and print

---
+++ file_io/main.h
@@ -0,0 +1,21 @@
+#ifndef __FILE_O/I__
+#define __FILE_O/I__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+
+
+#endif /*__FILE_O/I__*/
