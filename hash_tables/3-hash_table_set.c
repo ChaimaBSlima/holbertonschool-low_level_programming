8bb8e95556ebe81ec562939f5e05139070d93f50
ChaimaBSlima <chaimabslima@outlook.fr>
2023-12-07 08:58:43 +0100

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	i = key_index((const unsigned char *)key, ht->size);
 	ptr = ht->array[i];
 
 	while (ptr)
